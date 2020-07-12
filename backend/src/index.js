f05ae0d01198653bcdc81a32a8ea948bf7e28b96
William Koller <wkoller25@gmail.com>
2020-07-12 01:41:15 -0300

    feat: add configs express

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,19 @@ const morgam = require('morgan')
 const server = express()
 const route = require('./routes/routes')
 
+server.use(express.urlencoded)
+server.use(express.json())
 server.use(morgam('dev'))
 server.use(route)
 
-server.listen(process.env.PORT, () => {
-	console.log(`Server running on http://localhost:${process.env.PORT}`)
-})
+if (process.env.PORT || 3000) {
+	server.listen(process.env.PORT, () => {
+		console.log(
+			`Server running on http://localhost:${
+				process.env.PORT
+			} - on date the ${new Date()}`
+		)
+	})
+}
 
 module.exports = server
