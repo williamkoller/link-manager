d08d997e6e9d91f3f147f8d93a409027062fb1b1
William Koller <wkoller25@gmail.com>
2020-07-12 01:32:23 -0300

    feat: add route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
+require('dotenv').config()
 const express = require('express')
-
+const morgam = require('morgan')
 const server = express()
+const route = require('./routes/routes')
 
-server.listen(3000, () => {
-    console.log('Server running on port: 3000')
+server.use(morgam('dev'))
+server.use(route)
+
+server.listen(process.env.PORT, () => {
+	console.log(`Server running on http://localhost:${process.env.PORT}`)
 })
 
 module.exports = server
