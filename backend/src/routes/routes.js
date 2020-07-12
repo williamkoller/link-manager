de1caf095abc901c8805ab0912bce2bc7eab0948
William Koller <wkoller25@gmail.com>
2020-07-12 01:31:59 -0300

    feat: add routes

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,7 +1,9 @@
 const express = require('express')
-
 const route = express.Router()
+const signInController = require('../controllers/sign-in-controller')
+const signUpController = require('../controllers/sign-up-controller')
 
-route.get('/')
+route.get('/auth/sign-in', signInController.getSignIn)
+route.get('/auth/sign-up', signUpController.getSignUp)
 
 module.exports = route
