b6e7d4861e54afe27310ae785697d743dd039218
William Koller <wkoller25@gmail.com>
2020-07-14 01:33:44 -0300

    feat: add configs

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 const databaseConfig = require('../config/database')
-
 const connection = new Sequelize(databaseConfig)
 
+const Account = require('../models/Account')
+Account.init(connection)
 module.exports = connection
---
+++ backend/src/database/migrations/20200714031210-account.js
@@ -0,0 +1,34 @@
+'use strict'
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		return queryInterface.createTable('accounts', {
+			id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+				allowNull: false,
+			},
+			email: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			password: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			created_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			updated_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+		})
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('accounts')
+	},
+}
