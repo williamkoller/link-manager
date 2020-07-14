20752c01c3ae60043fc23f3e6890d8a39eb12ba3
William Koller <wkoller25@gmail.com>
2020-07-14 01:33:06 -0300

    feat: add Account Controller

--- backend/src/controllers/sign-up-controller.js
+++ backend/src/controllers/sign-up-controller.js
@@ -1,10 +1,25 @@
-exports.getSignUp = (request, response) => {
-	return new Promise((resolve, reject) => {
-		try {
-			const result = response.send({ message: 'sign up ' })
-			resolve(result)
-		} catch (err) {
-			reject(err)
-		}
-	})
+const Account = require('../models/Account')
+module.exports = {
+	store(request, response) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const { email, password } = request.body
+				const user = await Account.create({ email, password })
+
+				resolve(
+					response.status(200).json({
+						message: 'Usuário cadastrado com sucesso',
+						user,
+					})
+				)
+				setTimeout(() => {}, 2000)
+			} catch (err) {
+				reject(
+					response.status(400).json({
+						message: 'Não foi possivel realizar seu cadastro',
+					})
+				)
+			}
+		})
+	},
 }
