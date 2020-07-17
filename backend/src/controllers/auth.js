8a43642e0af6464aaa3e2fb9629bca4e0625f3fb
William Koller <wkoller25@gmail.com>
2020-07-16 23:15:26 -0300

    feat: add bcrypt

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,19 +1,22 @@
 const express = require('express')
-const { Account } = require('../models')
 const router = express.Router()
-
+const bcrypt = require('bcrypt')
+const { Account } = require('../models')
+const salt = 10
 router.get('/sign-in', (request, response) => {
 	return response.json('Sign in')
 })
 
-router.get('/sign-up', async (request, response) => {
+router.post('/sign-up', async (request, response) => {
 	const { email, password } = request.body
+
+	const hash = bcrypt.hashSync(password, salt)
 	const result = await Account.create({
 		email,
-		password,
+		password: hash,
 	})
 	console.log(result)
-	return response.json(result)
+	return response.json(result.email)
 })
 
 module.exports = router
