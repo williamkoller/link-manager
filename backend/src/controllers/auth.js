74aaa6cd6722e8fdf00aa38a45086dcfa985ef3f
William Koller <wkoller25@gmail.com>
2020-07-17 00:30:51 -0300

    feat: add condition if email exists

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
 const { Account } = require('../models')
+const { response } = require('express')
 const salt = 10
 router.get('/sign-in', (request, response) => {
 	return response.json('Sign in')
@@ -10,13 +11,25 @@ router.get('/sign-in', (request, response) => {
 router.post('/sign-up', async (request, response) => {
 	const { email, password } = request.body
 
+	const account = await Account.findOne({ where: { email } })
+	if (account) {
+		return response.json('Account already exists')
+	}
+
 	const hash = bcrypt.hashSync(password, salt)
-	const result = await Account.create({
+	const newAccount = await Account.create({
 		email,
 		password: hash,
 	})
-	console.log(result)
-	return response.json(result.email)
+	console.log(newAccount)
+	return response.json(newAccount)
 })
 
+router.get('/users', async (request, response) => {
+	const users = await Account.findAll()
+	if (users == '' && users == null) {
+		return response.json('Users not found')
+	}
+	return response.json(users)
+})
 module.exports = router
