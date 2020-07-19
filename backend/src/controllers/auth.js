3906de0f3085f3d808eac0e58d445da499088bde
William Koller <wkoller25@gmail.com>
2020-07-19 16:19:04 -0300

    feat: add method that deleted users

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -11,23 +11,73 @@ router.get('/sign-in', (request, response) => {
 })
 
 router.post('/sign-up', accountSignUp, async (request, response) => {
-	const { email, password } = request.body
+	return new Promise(async (resolve, reject) => {
+		try {
+			const { email, password } = request.body
 
-	const account = await Account.findOne({ where: { email } })
-	if (account) {
-		return response.jsonBadRequest(
-			null,
-			getMessage('account.signup.email_exists')
-		)
-	}
+			const account = await Account.findOne({ where: { email } })
+			if (account) {
+				return response.jsonBadRequest(
+					null,
+					getMessage('account.signup.email_exists')
+				)
+			}
 
-	const hash = bcrypt.hashSync(password, salt)
-	const newAccount = await Account.create({
-		email,
-		password: hash,
+			const hash = bcrypt.hashSync(password, salt)
+			
+			const newAccount = await Account.create({
+				email,
+				password: hash,
+
+			})
+			console.log(newAccount)
+			resolve(response.jsonOK(newAccount, getMessage('account.signup.success')))
+		} catch (err) {
+			reject(response.jsonBadRequest(err, getMessage('response.json_bad_request')))
+		}
 	})
-	console.log(newAccount)
-	return response.jsonOK(newAccount, getMessage('account.signup.success'))
+
+
+
+})
+
+router.delete('/users/:userId', (request, response) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const { userId } = request.params
+			const account = await Account.findOne({
+				where: {
+					id: userId
+				}
+			})
+			console.log(account, userId)
+			if (!account) {
+				reject(response.jsonBadRequest(null, getMessage('account.signup.users.not_found')))
+			}
+			await Account.destroy({
+				where: {
+					id: userId
+				}
+			})
+			resolve(response.jsonOK(null, getMessage('account.signup.users.delete')))
+		} catch (err) {
+			console.error(err)
+			reject(response.jsonBadRequest(err, getMessage('response.json_bad_request')))
+		}
+	})
+})
+
+router.get('/users', (request, response) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const users = await Account.findAll()
+			console.log(users)
+			resolve(response.jsonOK(users, getMessage('response.json_ok')))
+		} catch (err) {
+			reject(response.jsonBadRequest(err, getMessage('response.json_bad_request')))
+		}
+	})
+
 })
 
 module.exports = router
