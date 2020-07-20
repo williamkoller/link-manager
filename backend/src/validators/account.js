8d8210a8b695a4ed074b781f519d5348584fbf91
William Koller <wkoller25@gmail.com>
2020-07-20 20:04:24 -0300

    refactor: add dot env w path .env

--- backend/src/validators/account.js
+++ backend/src/validators/account.js
@@ -1,16 +1,39 @@
 const Joi = require('@hapi/joi')
 const { getValidatorError } = require('../helpers/validator')
 
+const rules = {
+	email: Joi.string().email().required(),
+	password: Joi.string().pattern(new RegExp('^[a-zA-Z-0-9]{3,30}$')),
+	password_confirmation: Joi.string()
+		.valid(Joi.ref('password'))
+		.required(),
+}
+const options = { abortEarly: false }
+
+const accountSignIn = (request, response, next) => {
+	const { email, password } = request.body
+	const schema = Joi.object({
+		email: rules.email,
+		password: rules.password
+
+	})
+	const { error } = schema.validate(
+		{ email, password },
+		options
+	)
+	if (error) {
+		const messages = getValidatorError(error, 'account.signin')
+		return response.jsonBadRequest(null, null, { error: messages })
+	}
+	next()
+}
 const accountSignUp = (request, response, next) => {
 	const { email, password, password_confirmation } = request.body
 	const schema = Joi.object({
-		email: Joi.string().email().required(),
-		password: Joi.string().pattern(new RegExp('^[a-zA-Z-0-9]{3,30}$')),
-		password_confirmation: Joi.string()
-			.valid(Joi.ref('password'))
-			.required(),
+		email: rules.email,
+		password: rules.password,
+		password_confirmation: rules.password_confirmation
 	})
-	const options = { abortEarly: false }
 	const { error } = schema.validate(
 		{ email, password, password_confirmation },
 		options
@@ -22,4 +45,4 @@ const accountSignUp = (request, response, next) => {
 	next()
 }
 
-module.exports = { accountSignUp }
+module.exports = { accountSignUp, accountSignIn }
