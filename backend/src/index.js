36fdc7b87f2b2ef02b7477518ab7615f81403ff5
William Koller <wkoller25@gmail.com>
2020-07-20 23:27:02 -0300

    feat: add linkController on server

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ const db = require('./models')
 const morgan = require('morgan')
 const server = express()
 const authController = require('./controllers/auth')
+const linkController = require('./controllers/link')
 const responseDefault = require('./middleware/response')
 
 server.use(responseDefault)
@@ -11,13 +12,14 @@ server.use(express.json())
 server.use(express.urlencoded({ extended: false }))
 server.use(morgan('dev'))
 server.use('/auth', authController)
+server.use('/link', linkController)
 
 db.sequelize.sync().then(() => {
 	if (process.env.PORT || 3001) {
 		server.listen(process.env.PORT, () => {
 			console.log(
 				`Listening on http://localhost:${
-					process.env.PORT
+				process.env.PORT
 				} - on date the ${new Date()}`
 			)
 		})
