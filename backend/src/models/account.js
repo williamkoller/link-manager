4ece0286e29abdc26d27535f40305352fd374e07
William Koller <wkoller25@gmail.com>
2020-07-20 23:27:22 -0300

    feat: add model link

--- backend/src/models/account.js
+++ backend/src/models/account.js
@@ -10,6 +10,11 @@ module.exports = (sequelize, DataTypes) => {
 			allowNull: false,
 		},
 	})
+
+	Account.associate = (models) => {
+		Account.hasMany(models.Link, { foreignKey: 'accountId' })
+	}
+	
 	Account.prototype.toJSON = function () {
 		const values = { ...this.get() }
 		delete values.password
