83a35645ba5aeeb2e652fb03ab9ade099cc32e2e
William Koller <wkoller25@gmail.com>
2020-07-21 13:13:32 -0300

    refactor: organize return w/ throw new Error

--- backend/src/controllers/link.js
+++ backend/src/controllers/link.js
@@ -4,89 +4,81 @@ const { getMessage } = require('../helpers/validator')
 const { Link } = require('../models')
 
 
-router.get('/', (request, response) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const links = await Link.findAll()
+router.get('/', async (request, response) => {
+    try {
+        const links = await Link.findAll()
 
-            resolve(response.jsonOK(links))
-        } catch (error) {
-            reject(error, response.jsonBadRequest(null, getMessage('response.json_bad_request')))
-        }
-    })
+        return response.jsonOK(links)
+    } catch (error) {
+        throw new Error(error, response.jsonBadRequest(null, getMessage('response.json_bad_request')))
+    }
+
+})
+
+router.get('/:id', async (request, response) => {
+    try {
+        const accountId = 2
+        const { id } = request.params
+        const link = await Link.findOne({ where: { id, accountId } })
+        if (!link) return response.jsonNotFound()
+        return response.jsonOK(link)
+    } catch (error) {
+        throw new Error(error, response.jsonBadRequest(null, getMessage('response.json_bad_request')))
+    }
+})
+
+router.post('/', async (request, response) => {
+    try {
+        const accountId = 2
+        const { label, url, isSocial } = request.body
+
+        const image = 'https://google.com/image.jpg'
+
+        const link = await Link.create({ label, url, isSocial, image, accountId })
+
+        return response.jsonOK(link)
+    } catch (error) {
+        throw new Error(error, response.jsonBadRequest(null, getMessage('response.json_bad_request')))
+    }
+
+})
+
+router.put('/:id', async (request, response) => {
+    try {
+        const accountId = 2
+        const { id } = request.params
+        const { body } = request
+        const fields = ['label', 'url', 'isSocial']
+
+        const link = await Link.findOne({ where: { id, accountId } })
+        if (!link) response.jsonNotFound()
+
+        fields.map(fiedlName => {
+            const newValue = body[fiedlName]
+            if (newValue) link[fiedlName] = newValue
+        })
+
+        await link.save()
+
+        return response.jsonOK(link)
+    } catch (error) {
+        throw new Error(error, response.jsonBadRequest(null, getMessage('response.json_bad_request')))
+    }
 
 
 })
 
-router.get('/:id', (request, response) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const accountId = 2
-            const { id } = request.params
-            const link = await Link.findOne({ where: { id, accountId } })
-            if (!link) return response.jsonNotFound()
-            resolve(response.jsonOK(link))
-        } catch (error) {
-            reject(error, response.jsonBadRequest(null, getMessage('response.json_bad_request')))
-        }
-    })
-})
+router.delete('/:id', async (request, response) => {
+    try {
+        const { id } = request.params
+        const link = await Link.findOne({ where: { id } })
+        if (!link) return response.jsonNotFound()
+        await Link.destroy({ where: { id } })
+        return response.jsonOK()
+    } catch (error) {
+        throw new Error(error, response.jsonBadRequest(null, getMessage('response.json_bad_request')))
+    }
 
-router.post('/', (request, response) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const accountId = 2
-            const { label, url, isSocial } = request.body
-
-            const image = 'https://google.com/image.jpg'
-
-            const link = await Link.create({ label, url, isSocial, image, accountId })
-
-            resolve(response.jsonOK(link))
-        } catch (error) {
-            reject(error, response.jsonBadRequest(null, getMessage('response.json_bad_request')))
-        }
-    })
-})
-
-router.put('/:id', (request, response) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const accountId = 2
-            const { id } = request.params
-            const { body } = request
-            const fields = ['label', 'url', 'isSocial']
-
-            const link = await Link.findOne({ where: { id, accountId } })
-            if (!link) response.jsonNotFound()
-
-            fields.map(fiedlName => {
-                const newValue = body[fiedlName]
-                if (newValue) link[fiedlName] = newValue
-            })
-
-            await link.save()
-
-            resolve(response.jsonOK(link))
-        } catch (error) {
-            reject(error, response.jsonBadRequest(null, getMessage('response.json_bad_request')))
-        }
-    })
-
-})
-
-router.delete('/:id', (request, response) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const { id } = request.params
-            const link = await Link.findOne({ where: { id } })
-            if (!link) return response.jsonNotFound()
-            await Link.destroy({ where: { id } })
-            resolve(response.jsonOK())
-        } catch (error) {
-            reject(error, response.jsonBadRequest(null, getMessage('response.json_bad_request')))
-        }
-    })
 })
 
 module.exports = router
