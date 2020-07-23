12cc384cb6f46fa06418f5419362d582d50a3449
William Koller <wkoller25@gmail.com>
2020-07-22 22:16:28 -0300

    feat: add env DB_HOST'

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,6 +6,7 @@ module.exports = {
 		password: process.env.DB_PASSWORD,
 		database: process.env.DATABASE,
 		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
 		dialect: 'mysql',
 		operatorsAliases: '0',
 	},
