5cb03cdc85948b34cd978a6d10249c199385aa1b
William Koller <wkoller25@gmail.com>
2020-08-06 22:26:11 -0300

    feat: add path excluded

--- backend/src/middleware/jwt.js
+++ backend/src/middleware/jwt.js
@@ -1,6 +1,12 @@
 const { verifyJwt } = require('../helpers/jwt')
 
 const checkJwt = (request, response, next) => {
+  const { url: path } = request
+
+  const excludedPaths = ['/auth/sign-in', '/auth/sign-up']
+  const isExcluded = !!excludedPaths.find((p) => p.startsWith(path))
+  if (isExcluded) return next()
+
   let token = request.headers.authorization
 
   token = token ? token.slice(7, token.length) : null
