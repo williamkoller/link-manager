025399787b4f24eccf72fb622352c53d08e76c22
William Koller <wkoller25@gmail.com>
2020-08-14 20:44:51 -0300

    feta: create routes w/ react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 
 const App = () => {
-  return <h1>Link</h1>
+  return (
+  <BrowserRouter>
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/sign-in">Sign in</Link></li>
+          <li><Link to="/sign-up">Sign up</Link></li>
+          <li><Link to="/manager/links/create">Create Link</Link></li>
+          <li><Link to="/manager/links/edit">Edit Link</Link></li>
+          <li><Link to="/manager/links">Links</Link></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/sign-in"><h1>Sign In</h1></Route>
+        <Route path="/sign-up"><h1>Sign Up</h1></Route>
+        <Route path="/manager/links/create"><h1>Create Link</h1></Route>
+        <Route path="/manager/links/edit"><h1>Edit Link</h1></Route>
+        <Route path="/manager/links"><h1>Links</h1></Route>
+        <Route path="/"><h1>Home</h1></Route>
+      </Switch>
+    </div>
+  </BrowserRouter>
+  )
 }
 
 export default App
