b45e29f2de9d09d29f4d835c8375c5a2d29ce32c
William Koller <wkoller25@gmail.com>
2020-08-14 23:19:07 -0300

    feat: add page create link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import SignIn from './Screens/SignIn'
 import SignUp from './Screens/SignUp'
 import Links from './Screens/Manager/Links'
+import Create from './Screens/Manager/Create'
 const App = () => {
   return (
   <BrowserRouter>
@@ -19,7 +20,7 @@ const App = () => {
       <Switch>
         <Route path="/sign-in"><SignIn /></Route>
         <Route path="/sign-up"><SignUp /></Route>
-        <Route path="/manager/links/create"><h1>Create Link</h1></Route>
+        <Route path="/manager/links/create"><Create /></Route>
         <Route path="/manager/links/edit"><h1>Edit Link</h1></Route>
         <Route path="/manager/links"><Links /></Route>
         <Route path="/"><h1>Home</h1></Route>
