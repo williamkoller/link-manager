3d852f132cd4a1e051ede6d50907a027ac4b2f46
William Koller <wkoller25@gmail.com>
2020-08-15 15:53:27 -0300

    feat: add pages home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,34 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import Home from './Screens/Home'
 import SignIn from './Screens/SignIn'
 import SignUp from './Screens/SignUp'
 import Links from './Screens/Manager/Links'
 import Create from './Screens/Manager/Create'
+import Edit from './Screens/Manager/Edit'
 const App = () => {
   return (
-  <BrowserRouter>
-    <div>
-      <nav>
-        <ul>
-          <li><Link to="/sign-in">Sign in</Link></li>
-          <li><Link to="/sign-up">Sign up</Link></li>
-          <li><Link to="/manager/links/create">Create Link</Link></li>
-          <li><Link to="/manager/links/edit">Edit Link</Link></li>
-          <li><Link to="/manager/links">Links</Link></li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route path="/sign-in"><SignIn /></Route>
-        <Route path="/sign-up"><SignUp /></Route>
-        <Route path="/manager/links/create"><Create /></Route>
-        <Route path="/manager/links/edit"><h1>Edit Link</h1></Route>
-        <Route path="/manager/links"><Links /></Route>
-        <Route path="/"><h1>Home</h1></Route>
-      </Switch>
-    </div>
-  </BrowserRouter>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul className="list-group list-group-horizontal">
+            <li className="list-group-item"><Link to="/sign-in">Sign in</Link></li>
+            <li className="list-group-item"><Link to="/sign-up">Sign up</Link></li>
+            <li className="list-group-item"><Link to="/manager/links/create">Create Link</Link></li>
+            <li className="list-group-item"><Link to="/manager/links/edit">Edit Link</Link></li>
+            <li className="list-group-item"><Link to="/manager/links">Links</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/sign-in"><SignIn /></Route>
+          <Route path="/sign-up"><SignUp /></Route>
+          <Route path="/manager/links/create"><Create /></Route>
+          <Route path="/manager/links/edit"><Edit /></Route>
+          <Route path="/manager/links"><Links /></Route>
+          <Route path="/"><Home /></Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   )
 }
 
